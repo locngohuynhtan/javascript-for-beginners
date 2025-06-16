896e8f943f27dffab0c58085b54ba207fc713a3f
Loc Ngo-Huynh-Tan <locngohuynhtan@gmail.com>
2025-06-16 10:07:55 +0700

    feat: get familiar with string in javascript

---
+++ string/string.js
@@ -0,0 +1,9 @@
+console.log(`String in JavaScript`);
+let a = `I'm a string`;
+let b = `We are 
+learning JavaScript`;
+console.log(a);
+console.log(b);
+
+console.log(4 + 5 + `a`); // 9a
+console.log(`a` + 4 + 5); // a45
